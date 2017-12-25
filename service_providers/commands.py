499f90b782b68b1b27a01631644f8c01dd0c8d91
Joseph Mancuso <idmann509@gmail.com>
2017-12-25 15:48:43 -0500

    fixed command line

--- service_providers/commands.py
+++ service_providers/commands.py
@@ -2,15 +2,20 @@
 
 import subprocess
 import sys
+import os
 from djangoproton.proton_settings import deployment
 
 def deploy():
-    print(deployment.DRIVERS[deployment.DEFAULT]['app'].lower())
-    output = subprocess.Popen(
-        ['heroku', 'git:remote', '-a', str(deployment.DRIVERS[deployment.DEFAULT]['app'].lower())], stdout=subprocess.PIPE).communicate()[0]
+    try:
+        output = subprocess.Popen(
+            ['heroku', 'git:remote', '-a', str(deployment.DRIVERS[deployment.DEFAULT]['app'].lower())], stdout=subprocess.PIPE).communicate()[0]
+    except FileNotFoundError:
+        raise Exception(
+            '\033[91mThe deploy command requires the Heroku CLI tool. Go to https://devcenter.heroku.com/articles/heroku-cli to learn more\033[0m'
+        )
     if not output:
         create_app = input(
-            "\n\033[92mApp doesn't exist for this account. Would you like to craft one?\033[0m \n\n[y/n] > ")  # Python 2
+            "\n\033[92mApp doesn't exist for this account. Would you like to create one?\033[0m \n\n[y/n] > ")  # Python 2
         if 'y' in create_app:
             subprocess.call(
                 ['heroku', 'create', deployment.DRIVERS[deployment.DEFAULT]['app'].lower()])
