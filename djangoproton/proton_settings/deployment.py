1ddff30002b3d8a9cb5bb65ee10627244475df81
Joseph Mancuso <idmann509@gmail.com>
2017-12-25 12:50:55 -0500

    added deployment setting

---
+++ djangoproton/proton_settings/deployment.py
@@ -0,0 +1,32 @@
+''' Deployment Settings Module '''
+import os
+
+'''
+|--------------------------------------------------------------------------
+| Default Driver
+|--------------------------------------------------------------------------
+|
+| The default driver will be used whenever a deployment command is used.
+| Your may specify the driver used when deploying.
+|
+| drivers supported: heroku
+'''
+
+DEFAULT = 'heroku'
+
+'''
+|--------------------------------------------------------------------------
+| Dictionary of Drivers
+|--------------------------------------------------------------------------
+|
+| All drivers can be put in here as a dictionary.
+|
+'''
+
+DRIVERS = {
+    'heroku': {
+        'username': 'username',
+        'password': 'password',
+        'app': 'Django Proton'
+    }
+}
